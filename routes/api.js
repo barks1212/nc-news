aa1afa59b31d82be550f8838d8851c3457b9b68d
barks1212 <willbarkerwork@gmail.com>
2018-02-06 11:45:58 +0000

    setup articles router

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {topicsRouter, articlesRouter, commentsRouter, usersRouter} = require('./index');
 
+
+
 router.use('/topics', topicsRouter)
 router.use('/articles', articlesRouter);
 router.use('/comments', commentsRouter);
---
+++ routes/articles.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getArticles,
+  getCommentsForArticle,
+  addCommentsForArticle,
+  updateArticleVote,
+  deleteArticle
+} = require('../controllers/articles');
+
+router.route('/')
+  .get(getArticles)
+
+router.route('/:article_id/comments')
+  .get(getCommentsForArticle)
+  .post(addCommentsForArticle);
+
+router.route('/:article_id')
+  .get(getArticles)
+  .put(updateArticleVote)
+  .delete(deleteArticle);
+
+
+module.exports = router;
