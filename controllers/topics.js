8de3ce010b750b67ff9632056e8b6ea09311e5ae
barks1212 <willbarkerwork@gmail.com>
2018-02-06 12:30:07 +0000

    getArticlesForTopic function

--- controllers/topics.js
+++ controllers/topics.js
@@ -10,3 +10,23 @@ function getAllTopics(req, res) {
         })
         .catch(console.error)
 }
+
+function getArticlesForTopic(req, res) {
+  let everyArticle
+  console.log(req.params.topic)
+  Articles.find({ belongs_to: req.params.topic }).lean()
+      .then((allArticles) => {
+          everyArticle = allArticles
+          let commentCount = allArticles.map((article) => {
+              return Comments.find({ belongs_to: article._id }).count();
+          })
+          return Promise.all(commentCount)
+      })
+      .then((comments) => {
+          everyArticle.forEach(function (element, i) {
+              element.comments = comments[i];
+          });
+          res.status(200).json(everyArticle);
+      })
+      .catch(console.error)
+}
