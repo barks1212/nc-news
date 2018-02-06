377c53ab0c436d042ab9c3f536aba839360e9ff0
barks1212 <willbarkerwork@gmail.com>
2018-02-06 11:15:07 +0000

    setup index file for routes

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const topicsRouter = require('./topics');
+const articlesRouter = require('./articles');
+const commentsRouter = require('./comments');
+const usersRouter = require('./users');
+
+module.exports = {topicsRouter, articlesRouter, commentsRouter, usersRouter};
