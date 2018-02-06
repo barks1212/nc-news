b0c584593cadd63f3acdbd4d02b1aab7b7c167f5
barks1212 <willbarkerwork@gmail.com>
2018-02-06 11:46:33 +0000

    setup comments router

---
+++ routes/comments.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const {getAllComments, updateCommentVote, deleteComment}  = require('../controllers/comments');
+
+
+router.route('/')
+  .get(getAllComments)
+
+router.route('/:comment_id')
+  .get(getAllComments)
+  .put(updateCommentVote)
+  .delete(deleteComment);
+
+module.exports = router;
