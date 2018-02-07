8dd827ae144949d429ac2bb3693de19f6ed2f03e
barks1212 <willbarkerwork@gmail.com>
2018-02-07 11:30:38 +0000

    comment endpoints fully tested

--- spec/comments.spec.js
+++ spec/comments.spec.js
@@ -24,40 +24,49 @@ describe('api/comments', function () {
           })
       });
     });
-    it('gets comments by id with status 200', () => {
-      return request
-        .get('/api/comments/5a79cd9e39d1b52e5f2ac3bc')
-        .expect(200)
-        .then(res => {
-          console.log(res.body)
-          expect(res.body.comments).to.be.an('array');
-          expect(res.body.comments[0].created_by).to.equal('weegembump');
-        })
+    describe('/:commentid', () => {
+      it('gets comments by id with status 200', () => {
+        return request
+          .get('/api/comments/5a79cd9e39d1b52e5f2ac3bc')
+          .expect(200)
+          .then(res => {
+            console.log(res.body)
+            expect(res.body.comments).to.be.an('array');
+            expect(res.body.comments[0].created_by).to.equal('weegembump');
+          })
+      });
     });
   });
 
   describe('PUT methods', () => {
-    it.only('change the vote and 202 status', () => {
-      let votes;
-      request
-        .get('/api/comments/5a79cd9e39d1b52e5f2ac3bc')
-        .then(res => {
-          votes = res.body.votes;
-        });
-      return request
-        .put('/api/comments/5a79cd9e39d1b52e5f2ac3bc?vote=up')
-        .expect(202)
-        .then(res => {
-          expect(res.body[0].votes).to.not.equal(votes);
-        });
+    describe('/:commentid', () => {
+      it('change the vote and 202 status', () => {
+        let votes;
+        request
+          .get('/api/comments/5a79cd9e39d1b52e5f2ac3bc')
+          .then(res => {
+            votes = res.body.votes;
+          });
+        return request
+          .put('/api/comments/5a79cd9e39d1b52e5f2ac3bc?vote=up')
+          .expect(202)
+          .then(res => {
+            expect(res.body[0].votes).to.not.equal(votes);
+          });
+      });
     });
   });
-  it('delete the comment and 200 status', () => {
-    return request
-      .delete('/api/comments/5a3d2c9456ce3f044853cbe5')
-      .expect(202)
-      .then(res => {
-        expect(res.body).to.equal('Comment deleted');
+
+  describe('DELETE methods', () => {
+    describe('/:commentid', () => {
+      it('delete the comment and 202 status', () => {
+        return request
+          .delete('/api/comments/5a79cd9e39d1b52e5f2ac3bb')
+          .expect(202)
+          .then(res => {
+            expect(res.body).to.equal('Comment deleted');
+          });
       });
+    });
   });
 });
